7a30e902960ac676f6b83038f30c2de010901180
Prakash Ranabhat <ranabhatprakash72@gmail.com>
2020-05-26 11:42:04 +1000

    Express Simple REST

--- node-express/index.js
+++ node-express/index.js
@@ -1,10 +1,28 @@
-const express = require('express'),
-    http = require('http');
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/foods', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('content-type', 'text/plain');
+    next();
+});
+
+app.get('/foods', (req, res, next) => {
+    res.end('Will send all the foods to you');
+});
+app.get('/foods/:foodId', (req, res, next) =>{
+    res.end('Will send details of the food:'
+    + req.params.foodId+ 'to you.');
+});
 
 app.use((req, res, next) => {
     console.log(req.headers);
